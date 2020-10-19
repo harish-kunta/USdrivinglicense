3fca3f80d7927c07dd671aac570f99b7286103fa
HarishKunta <33089746+harishkunta@users.noreply.github.com>
2020-10-19 00:28:26 -0400

    added help button

--- app/src/main/java/com/harish/usdrivinglicensetest/MainActivity.java
+++ app/src/main/java/com/harish/usdrivinglicensetest/MainActivity.java
@@ -1,7 +1,9 @@
 package com.harish.usdrivinglicensetest;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +13,7 @@ import android.widget.ScrollView;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.analytics.FirebaseAnalytics;
 
 public class MainActivity extends AppCompatActivity {
@@ -47,6 +50,64 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent bookmarksIntent = new Intent(MainActivity.this, BookmarkActivity.class);
                 startActivity(bookmarksIntent);
+            }
+        });
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder
+                        = new AlertDialog
+                        .Builder(MainActivity.this);
+
+                // Set the message show for the Alert time
+                builder.setMessage(R.string.help_message);
+
+                // Set Alert Title
+                builder.setTitle("Help !");
+
+                // Set Cancelable false
+                // for when the user clicks on the outside
+                // the Dialog Box then it will remain show
+                builder.setCancelable(false);
+
+                // Set the positive button with yes name
+                // OnClickListener method is use of
+                // DialogInterface interface.
+
+                builder
+                        .setPositiveButton(
+                                "OK",
+                                new DialogInterface
+                                        .OnClickListener() {
+
+                                    @Override
+                                    public void onClick(DialogInterface dialog,
+                                                        int which)
+                                    {
+
+                                        // When the user click yes button
+                                        // then app will close
+                                        dialog.cancel();
+                                    }
+                                });
+
+                // Set the Negative button with No name
+                // OnClickListener method is use
+                // of DialogInterface interface.
+
+
+                // Create the Alert dialog
+                final AlertDialog alertDialog = builder.create();
+                alertDialog.setOnShowListener( new DialogInterface.OnShowListener() {
+                    @Override
+                    public void onShow(DialogInterface arg0) {
+                        alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(getResources().getColor(R.color.colorPrimary));
+                    }
+                });
+
+                // Show the Alert Dialog box
+                alertDialog.show();
             }
         });
     }
